6f3d9ce242134976e0a83f7031d9c64980114715
pennabadiumamaheswarreddy <pennabadiumamaheswarreddy@gmail.com>
2025-11-01 21:15:07 +0530

    terraform

---
+++ outputs.tf
@@ -0,0 +1,10 @@
+output "public_ip" {
+  value       = aws_instance.this.public_ip
+  description = "public IP of the instance created"
+}
+
+output "private_ip" {
+  value       = aws_instance.this.private_ip
+  description = "public IP of the instance created"
+}
+
---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "ami_id" {
+  type        = string
+  description = "this is the AMI used for creating EC2 instance"
+}
+
+variable "instance_type" {
+    type      = string
+    description = "Instance type used for creating ec2 instance"
+    validation {
+        condition = contains(["t3.micro","t3.small","t3.micro"], var.instance_type)
+        error_message = "Please select either t3 micro or small or medium"
+    }
+}
+
+variable "sg_ids" {
+    type = list
+}
+
+#optional
+variable "tags"{
+    type = map
+    default = {}
+}
